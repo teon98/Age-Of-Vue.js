d7ad064ee4494a3c1364686222b6672b7030ca82
Tae Young <49816869+teon98@users.noreply.github.com>
2023-11-02 17:38:04 +0900

    Vue 기초 강의 수강 완료

---
+++ vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username" />
+      </div>
+      <div>
+        <label for="password">pw:</label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      //e.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
