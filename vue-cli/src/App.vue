dbefe1f5021d2ce5a9c005a0b10f196ccf451ca0
Tae Young <49816869+teon98@users.noreply.github.com>
2023-11-02 10:57:45 +0900

    [센션10] 프로젝트 생성 도구 - Vue CLI

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- 컴포넌트 태그를 등록한 것과 같다.-->
+    <HelloWorld v-bind:msg="message" />
+    <!-- 컴포넌트 명명법 종류 -->
+    <!--
+      <hello-world></hello-world>
+      <HelloWorld></HelloWorld>
+      <HelloWorld/>
+    -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
+  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
   name: 'App',
+  data() {
+    return { message: '안녕' };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  props: {
+    //배열로 전달해줬던 props값을 props validation으로 type까지 보낸다.
+    msg: String,
+  },
+};
 </script>
 
 <style>
